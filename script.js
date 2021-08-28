c3959f97e24b776c1afba76a659513a9531ba6c2
dybovik <sandrvik@gmail.com>
2021-08-28 17:32:31 +0300

    Complete second task

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+/*** Task 1 ***/
+
 // Case 1
 {
     const name = 'Alex';
-    let message = (typeof (name) === 'string') ? `Hello, ${name}` : `Ошибка, не тот тип данных`;
+    let message = typeof name === 'string' ? `Hello, ${name}` : `Ошибка, не тот тип данных`;
 
     console.log(message);
 }
@@ -9,7 +11,18 @@
 // Case 2
 {
     const name = 123;
-    let message = (typeof (name) === 'string') ? `Hello, ${name}` : `Ошибка, не тот тип данных`;
+    let message = typeof name === 'string' ? `Hello, ${name}` : `Ошибка, не тот тип данных`;
 
     console.log(message);
 }
+
+/*** Task 2 ***/
+
+console.log(typeof 2);
+console.log(typeof 10n);
+console.log(typeof 'string');
+console.log(typeof true);
+console.log(typeof { name: 'Alex' });
+console.log(typeof null);
+console.log(typeof Symbol());
+console.log(typeof undefined);

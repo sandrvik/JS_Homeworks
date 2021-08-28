7f6c1b8edf7977b87888c39d204a1ab8630b1faa
dybovik <sandrvik@gmail.com>
2021-08-28 17:04:04 +0300

    Add second case

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // Case 1
+{
+    const name = 'Alex';
+    let message = (typeof (name) === 'string') ? `Hello, ${name}` : `Ошибка, не тот тип данных`;
 
-let name = 'Alex';
-let message = (typeof (name) === 'string') ? `Hello, ${name}` : `Ошибка, не тот тип данных`;
+    console.log(message);
+}
 
-console.log(message);
+// Case 2
+{
+    const name = 123;
+    let message = (typeof (name) === 'string') ? `Hello, ${name}` : `Ошибка, не тот тип данных`;
+
+    console.log(message);
+}

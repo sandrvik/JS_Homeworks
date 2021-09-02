f0a430118e6ffe641838c8adc42aef6f066a88a7
dybovik <sandrvik@gmail.com>
2021-09-02 12:49:59 +0300

    Complete task 1

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 /***** Task 1 *****/
 
+function Animal(name, age, colour) {
+    if (!new.target) {
+        return new Animal(name, age, colour);
+    }
 
+    this.name = name;
+    this.age = age;
+    this.colour = colour;
+}
+
+let cat = new Animal('bars', 4, 'black'); // cat --> { name: 'bars', age: 4, colour: 'black' } 
+
+let dog = Animal('rex', 14, 'gold'); // dog --> { name: 'rex', age: 14, colour: 'gold' } 
 
 /***** Task 2 *****/
 

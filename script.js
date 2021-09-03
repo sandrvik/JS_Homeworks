7ae924394ac563cd6161e300e3af8504f511cb8e
dybovik <sandrvik@gmail.com>
2021-09-03 14:38:54 +0300

    Complete task 2

--- script.js
+++ script.js
@@ -10,7 +10,15 @@ removeUser(users, 1) // ['Vasya', 'Alexey']
 
 /***** Task 2 *****/
 
+function makeDiv(colour, destination) {
+    const div = document.createElement('div');
+    div.classList = `square ${colour}`;
+    destination.append(div);
+}
 
+makeDiv('blue', sq_container);
+makeDiv('red', sq_container);
+makeDiv('green', sq_container);
 
 /***** Task 3 *****/
 

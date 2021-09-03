c83fa30ac03670034aa8836aba97afb8d7b255a4
dybovik <sandrvik@gmail.com>
2021-09-03 13:04:12 +0300

    Complete task 1

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 /***** Task 1 *****/
 
+function removeUser(arr, n) {
+    arr.splice(n, 1);
+    return arr;
+}
 
+const users = ['Vasya', 'Petya', 'Alexey'];
+removeUser(users, 1) // ['Vasya', 'Alexey']
 
 /***** Task 2 *****/
 

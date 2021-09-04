ac4aad4e8c3eae4cd46f27374a32bd1b3724311e
dybovik <sandrvik@gmail.com>
2021-09-04 14:13:46 +0300

    Complete task 1

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 /***** Task 1 *****/
 
+const div = document.createElement('div');
+div.innerText = 'Click';
+document.body.append(div);
 
+const img = document.createElement('img');
+img.src = 'https://overcoder.net/img/1/17/1/160042.png';
+
+let id;
+let size = 100;
+
+function makeBigger(speed, maxSize) {
+    if (size < 200) {
+        id = setInterval(() => {
+            size++;
+            if (img.style.width === `${maxSize}px`) {
+                clearInterval(id);
+            }
+            img.style.width = `${size}px`
+        }, speed);
+    }
+}
+
+div.addEventListener('click', () => document.body.append(img));
+img.addEventListener('click', (e) => e.target.remove());
+
+img.addEventListener('mouseover', () => makeBigger(20, 200));
+img.addEventListener('mouseout', () => clearInterval(id));
 
 /***** Task 2 *****/
 

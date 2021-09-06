ed7eb83721681ad13d0d56fe3bb76e11dfdbba77
dybovik <sandrvik@gmail.com>
2021-09-06 20:05:53 +0300

    Complete task 1

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 /***** Task 1 *****/
 
+function fibo(n) {
+    if (n <= 1) {
+        return n
+    } else return fibo(n - 1) + fibo(n - 2)
+}
 
+function fiboFast(n) {
+    let a = 1;
+    let b = 1;
+    let c;
+    for (let i = 3; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
 
 /***** Task 2 *****/
 

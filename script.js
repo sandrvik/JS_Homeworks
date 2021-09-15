d6642f7cc5a34f0171ad527972d1fb5e87097016
dybovik <sandrvik@gmail.com>
2021-09-15 19:10:19 +0300

    Complete task 1

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 /**** Task1 ****/
 
+const me = {
+    name: 'Alex',
+    age: 29
+}
 
+function objToStrings(object) {
+    let arr = [];
+    for (let key in object) {
+        arr.push(`${key}=${object[key]}`)
+    }
+    return arr;
+}
+
+const arrayForCookies = objToStrings(me);
+
+arrayForCookies.forEach(element => document.cookie = element);
+
+console.log(document.cookie)
 
 /**** Task2 ****/
 
